f8a14713389c2a6810355a2be042fbc9f4a85f3b
FractalMandala <i.amrit.p@gmail.com>
2026-08-31 20:02:14 +0530

    yep gotta go

--- temp/svocs-main/packages/create-svocs-docs/template/src/routes/docs/[...slug]/+page.ts
+++
@@ -1,8 +0,0 @@
-import type { PageLoad } from './$types';
-import { loadDocComponent } from '$lib/core/content';
-
-export const prerender = true;
-
-export const load: PageLoad = async ({ data }) => {
-	return { ...data, Content: await loadDocComponent(data.entry.slug) };
-};
